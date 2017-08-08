ad2cf726817c9dc32e8f6dd1fde31a4d933855a2
Pavel Kasymbekov <Kasymbekoff@yandex.ru>
2017-08-08 17:39:04 +0700

    Added menu and status bar

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,11 +1,193 @@
+#include <QtWidgets/QLabel>
+#include <QtWidgets/QVBoxLayout>
+#include <QtWidgets/QMenu>
+#include <QtWidgets/QAction>
+#include <QtWidgets/QActionGroup>
+#include <QContextMenuEvent>
+#include <QMessageBox>
+#include <QApplication>
+#include <QtWidgets/QMenuBar>
+#include <QtWidgets/QStatusBar>
+
 #include "mainwindow.h"
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
 {
+
+    QWidget* centralWidget = new QWidget;
+    setCentralWidget(centralWidget);
+
+    // TODO: Убрать
+    QWidget* filler = new QWidget;
+    filler->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Expanding);
+
+    statusBar = new QStatusBar;
+
+    QVBoxLayout* layout = new QVBoxLayout;
+    layout->setMargin(5);
+    layout->addWidget(filler);
+    layout->addWidget(statusBar);
+    centralWidget->setLayout(layout);
+
+    createActions();
+    createMenus();
 }
 
 MainWindow::~MainWindow()
 {
+
+}
+
+#ifndef QT_NO_CONTEXTMENU
+void MainWindow::contextMenuEvent(QContextMenuEvent* event){
+    QMenu menu(this);
+    menu.addAction(cutAct);
+    menu.addAction(copyAct);
+    menu.addAction(pasteAct);
+    menu.exec(event->globalPos());
+}
+#endif//QT_NO_CONTEXTMENU
+
+void MainWindow::newFile(){
+    statusBar->showMessage(tr("File|New"));
+}
+
+void MainWindow::open(){
+    statusBar->showMessage(tr("File|Open"));
+}
+
+void MainWindow::save(){
+    statusBar->showMessage(tr("File|Save"));
+}
+
+void MainWindow::saveAs(){
+    statusBar->showMessage(tr("File|Save As"));
+}
+
+void MainWindow::undo(){
+    statusBar->showMessage(tr("Edit|Undo"));
+}
+
+void MainWindow::redo(){
+    statusBar->showMessage(tr("Edit|Redo"));
+}
+
+void MainWindow::cut(){
+    statusBar->showMessage(tr("Edit|Cut"));
+}
+
+void MainWindow::paste(){
+    statusBar->showMessage(tr("Edit|paste"));
+}
+
+void MainWindow::copy(){
+    statusBar->showMessage(tr("Edit|copy"));
+}
+
+void MainWindow::selectAll(){
+    statusBar->showMessage(tr("Edit|select all"));
+}
+
+void MainWindow::about(){
+    statusBar->showMessage(tr("Help|About"));
+}
+
+void MainWindow::aboutQt(){
+    statusBar->showMessage(tr("Help|About Qt"));
+}
+
+void MainWindow::createActions(){
+
+    newAct = new QAction(tr("&New"), this);
+    newAct->setShortcuts(QKeySequence::New);
+    newAct->setStatusTip(tr("Create a new file"));
+    connect(newAct, &QAction::triggered, this, &MainWindow::newFile);
+
+    openAct = new QAction(tr("&Open"), this);
+    openAct->setShortcut(QKeySequence::Open);
+    openAct->setStatusTip(tr("Open an existing file"));
+    connect(openAct, &QAction::triggered, this, &MainWindow::open);
+
+    saveAct = new QAction(tr("Save"), this);
+    saveAct->setShortcuts(QKeySequence::Save);
+    saveAct->setStatusTip(tr("Save this document to disk"));
+    connect(saveAct, &QAction::triggered, this, &MainWindow::save);
+
+    saveAsAct = new QAction(tr("Save as..."), this);
+    saveAsAct->setShortcuts(QKeySequence::SaveAs);
+    saveAsAct->setStatusTip(tr("Save this document as..."));
+    connect(saveAsAct, &QAction::triggered, this, &MainWindow::saveAs);
+
+    exitAct = new QAction(tr("E&xit"), this);
+    exitAct->setShortcuts(QKeySequence::Quit);
+    exitAct->setStatusTip(tr("Exit the application"));
+    connect(exitAct, &QAction::triggered, this, &QWidget::close);
+
+    undoAct = new QAction(tr("&Undo"), this);
+    undoAct->setShortcuts(QKeySequence::Undo);
+    undoAct->setStatusTip(tr("Undo the last operation"));
+    connect(undoAct, &QAction::triggered, this, &MainWindow::undo);
+
+    redoAct = new QAction(tr("&Redo"), this);
+    redoAct->setShortcuts(QKeySequence::Redo);
+    redoAct->setStatusTip(tr("Redo the last operation"));
+    connect(redoAct, &QAction::triggered, this, &MainWindow::redo);
+
+    cutAct = new QAction(tr("Cu&t"), this);
+    cutAct->setShortcuts(QKeySequence::Cut);
+    cutAct->setStatusTip(tr("Cut current selection's contentsto the clipboard"));
+    connect(cutAct, &QAction::triggered, this, &MainWindow::cut);
+
+    copyAct = new QAction(tr("&Copy"), this);
+    copyAct->setShortcuts(QKeySequence::Copy);
+    copyAct->setStatusTip(tr("Copy current selection's"
+                             "contents to the clipboard"));
+    connect(copyAct, &QAction::triggered, this, &MainWindow::copy);
+
+    pasteAct = new QAction(tr("&Paste"), this);
+    pasteAct->setShortcuts(QKeySequence::Paste);
+    pasteAct->setStatusTip(tr("Paste the clipboard's contents"
+                              "into the current selection"));
+    connect(pasteAct, &QAction::triggered, this, &MainWindow::paste);
+
+    selectAllAct = new QAction(tr("Select All"), this);
+    selectAllAct->setShortcuts(QKeySequence::SelectAll);
+    selectAllAct->setStatusTip(tr("Select all"));
+    connect(selectAllAct, &QAction::triggered, this, &MainWindow::selectAll);
+
+    aboutAct = new QAction(tr("&About"), this);
+    aboutAct->setStatusTip(tr("Show the application's About box"));
+    connect(aboutAct, &QAction::triggered, this, &MainWindow::about);
+
+    aboutQtAct = new QAction(tr("About &Qt"), this);
+    aboutQtAct->setStatusTip(tr("Show the Qt library's About box"));
+    connect(aboutQtAct, &QAction::triggered, qApp, &QApplication::aboutQt);
+    connect(aboutQtAct, &QAction::triggered, this, &MainWindow::aboutQt);
+
+}
+
+void MainWindow::createMenus(){
+
+    fileMenu = menuBar()->addMenu(tr("&File"));
+    fileMenu->addAction(newAct);
+    fileMenu->addAction(openAct);
+    fileMenu->addAction(saveAct);
+    fileMenu->addAction(saveAsAct);
+    fileMenu->addSeparator();
+    fileMenu->addAction(exitAct);
+
+    editMenu = menuBar()->addMenu(tr("&Edit"));
+    editMenu->addAction(undoAct);
+    editMenu->addAction(redoAct);
+    editMenu->addSeparator();
+    editMenu->addAction(cutAct);
+    editMenu->addAction(copyAct);
+    editMenu->addAction(pasteAct);
+    editMenu->addSeparator();
+
+    helpMenu = menuBar()->addMenu(tr("&Help"));
+    helpMenu->addAction(aboutAct);
+    helpMenu->addAction(aboutQtAct);
 
 }
--- mainwindow.h
+++ mainwindow.h
@@ -3,6 +3,8 @@
 
 #include <QMainWindow>
 
+class QActionGroup;
+
 class MainWindow : public QMainWindow
 {
     Q_OBJECT
@@ -10,6 +12,52 @@ class MainWindow : public QMainWindow
 public:
     MainWindow(QWidget *parent = 0);
     ~MainWindow();
+
+protected:
+#ifndef QT_NO_CONTEXTMENU
+    void contextMenuEvent(QContextMenuEvent* event) override;
+#endif//QT_NO_CONTEXTMENU
+
+private slots:
+    void newFile();
+    void open();
+    void save();
+    void saveAs();
+    void undo();
+    void redo();
+    void cut();
+    void paste();
+    void copy();
+    void selectAll();
+    void about();
+    void aboutQt();
+
+private:
+    void createActions();
+    void createMenus();
+
+    QStatusBar* statusBar;
+
+    QMenu* fileMenu;
+    QMenu* editMenu;
+    QMenu* helpMenu;
+
+    QAction* newAct;
+    QAction* openAct;
+    QAction* saveAct;
+    QAction* saveAsAct;
+    QAction* exitAct;
+
+    QAction* undoAct;
+    QAction* redoAct;
+    QAction* cutAct;
+    QAction* pasteAct;
+    QAction* copyAct;
+    QAction* selectAllAct;
+
+    QAction* aboutAct;
+    QAction* aboutQtAct;
+
 };
 
 #endif // MAINWINDOW_H
